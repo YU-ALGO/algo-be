96a8ad07c9a6c2afe9ef8f6c26bef1dddff8e88f
munis <aaaa6658@naver.com>
2022-07-13 17:21:25 +0900

    Init Commit

---
+++ src/main/java/com/stock/yu/downbitbe/DownbitBeApplication.java
@@ -0,0 +1,13 @@
+package com.stock.yu.downbitbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownbitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DownbitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stock/yu/downbitbe/DownbitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.yu.downbitbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownbitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
