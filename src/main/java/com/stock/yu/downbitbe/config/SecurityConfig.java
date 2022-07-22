db6b66d077011de9b00196ffc66be47b1a713dd7
Capoomaru <anycar8515@ynu.ac.kr>
2022-07-22 21:17:59 +0900

    0722

---
+++ src/main/java/com/stock/yu/downbitbe/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.stock.yu.downbitbe.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@Log4j2
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+        http.formLogin();
+        http.csrf().disable();
+        http.logout();
+        /*
+        로그아웃 후 연결될 링크
+        logoutSuccessUrl()
+        logoutUrl()
+         */
+
+        return http.build();
+    }
+
+
+
+
+
+
+}
