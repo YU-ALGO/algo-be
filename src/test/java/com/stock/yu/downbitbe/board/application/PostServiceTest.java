72cfa1bd135cce3a80bd15669447cfeacd9b5787
munis <aaaa6658@naver.com>
2022-09-07 21:46:20 +0900

    post service test code add

--- src/test/java/com/stock/yu/downbitbe/board/application/PostServiceTest.java
+++ src/test/java/com/stock/yu/downbitbe/board/application/PostServiceTest.java
@@ -14,7 +14,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
